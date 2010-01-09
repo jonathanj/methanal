dde3324bca8fb85f4b36b4fef07f5d052c616d59
Jonathan Jacobs <korpse@slipgate.za.net>
2010-01-09 15:29:56 +0200

    Mock HTML table elements.

--- methanal/js/Methanal/Tests/MockBrowser.js
+++ methanal/js/Methanal/Tests/MockBrowser.js
@@ -58,6 +58,98 @@ Methanal.Tests.MockBrowser.Element.subclass(
 
 
 /**
+ * HTMLTableRowElement mock implementation.
+ */
+Methanal.Tests.MockBrowser.Element.subclass(
+    Methanal.Tests.MockBrowser, 'MockHTMLTableRowElement').methods(
+    function __init__(self) {
+        Methanal.Tests.MockBrowser.MockHTMLTableRowElement.upcall(
+            self, '__init__', 'tr');
+        self.cells = [];
+    },
+
+
+    /**
+     * Insert an empty TD cell into this row. If index is -1 or equal to the
+     * number of cells, the new cell is appended.
+     */
+    function insertCell(self, index) {
+        var cell = document.createElement('td');
+        if (index == -1) {
+            self.cells.push(cell);
+        } else {
+            self.cells.splice(index, 0, cell);
+        }
+        return cell;
+    });
+
+
+
+/**
+ * HTMLTableSectionElement mock implementation.
+ */
+Methanal.Tests.MockBrowser.Element.subclass(
+    Methanal.Tests.MockBrowser, 'MockHTMLTableSectionElement').methods(
+    function __init__(self, tagName) {
+        Methanal.Tests.MockBrowser.MockHTMLTableSectionElement.upcall(
+            self, '__init__', tagName);
+        self.rows = [];
+    },
+    
+    
+    /**
+     * Insert a new empty row in the table. The new row is inserted immediately
+     * before and in the same section as the current indexth row in the table.
+     * If index is -1 or equal to the number of rows, the new row is appended.
+     * In addition, when the table is empty the row is inserted into a TBODY
+     * which is created and inserted into the table.
+     */
+    function insertRow(self, index) {
+        var row = document.createElement('tr');
+        if (index == -1) {
+            self.rows.push(row);
+        } else {
+            self.rows.splice(index, 0, row);
+        }
+        return row;
+    });
+
+
+
+/**
+ * HTMLTableElement mock implementation.
+ */
+Methanal.Tests.MockBrowser.Element.subclass(
+    Methanal.Tests.MockBrowser, 'MockHTMLTableElement').methods(
+    function __init__(self) {
+        Methanal.Tests.MockBrowser.MockHTMLTableElement.upcall(
+            self, '__init__', 'table');
+        self.deleteTHead();
+        self.tBodies = [document.createElement('tbody')];
+    },
+
+
+    /**
+     * Delete the header from the table, if one exists.
+     */
+    function deleteTHead(self) {
+        self.tHead = null;
+    },
+    
+
+    /**
+     * Create a table header row or return an existing one.
+     */
+    function createTHead(self) {
+        if (self.tHead !== null) {
+            return self.tHead;
+        }
+        self.tHead = document.createElement('thead');
+    });
+
+
+
+/**
  * HTMLDocument mock implementation.
  */
 Divmod.MockBrowser.Document.subclass(
@@ -81,7 +173,7 @@ Divmod.MockBrowser.Document.subclass(
     function createElement(self, tagName) {
         var el;
         if (self._elementTags[tagName]) {
-            el = self._elementTags[tagName]();
+            el = self._elementTags[tagName](tagName);
         } else {
             el = Methanal.Tests.MockBrowser.Element(tagName);
         }
@@ -98,5 +190,13 @@ if (document instanceof Divmod.MockBrowser.Document) {
         document = Methanal.Tests.MockBrowser.Document();
         document.registerElementTag(
             'select', Methanal.Tests.MockBrowser.MockHTMLSelectElement);
+        document.registerElementTag(
+            'table', Methanal.Tests.MockBrowser.MockHTMLTableElement);
+        document.registerElementTag(
+            'thead', Methanal.Tests.MockBrowser.MockHTMLTableSectionElement);
+        document.registerElementTag(
+            'tbody', Methanal.Tests.MockBrowser.MockHTMLTableSectionElement);
+        document.registerElementTag(
+            'tr', Methanal.Tests.MockBrowser.MockHTMLTableRowElement);
     }
 }
