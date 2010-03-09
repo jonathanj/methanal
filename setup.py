628cbacfc736f5929a640ecebddc2d86498f6d1b
Jonathan Jacobs <korpse@slipgate.za.net>
2010-03-09 15:26:03 +0200

    Update maintainer info in setup.py.

--- setup.py
+++ setup.py
@@ -5,8 +5,8 @@ import methanal
 autosetup(
     name='Methanal',
     version=methanal.version.short(),
-    maintainer='Tristan Seligmann',
-    maintainer_email='mithrandi@mithrandi.za.net',
+    maintainer='Tristan Seligmann / Jonathan Jacobs',
+    maintainer_email='mithrandi@mithrandi.za.net / korpse@slipgate.za.net',
     license='MIT',
     platforms=['any'],
     description='A web forms library for Mantissa',
