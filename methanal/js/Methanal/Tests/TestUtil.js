59dc92bb6ab84b2e1fd157b71e4affa163558307
Jonathan Jacobs <korpse@slipgate.za.net>
2010-11-30 14:41:25 +0200

    Implement assertCases helper.

--- methanal/js/Methanal/Tests/TestUtil.js
+++ methanal/js/Methanal/Tests/TestUtil.js
@@ -192,14 +192,7 @@ Methanal.Tests.Util.TestCase.subclass(
             ['123abc', undefined],
             ['abc123', undefined],
             ['0x123', undefined]];
-
-        for (var i = 0; i < CASES.length; ++i) {
-            var input = CASES[i][0];
-            var expected = CASES[i][1];
-            var actual = Methanal.Util.strToInt(input);
-            self.assert(expected === actual, 'input = ' + input +
-                ' :: expected = ' + expected + ' :: actual = ' + actual);
-        }
+        Methanal.Tests.Util.assertCases(self, Methanal.Util.strToInt, CASES);
     },
 
 
@@ -209,21 +202,15 @@ Methanal.Tests.Util.TestCase.subclass(
      */
     function test_strToFloat(self) {
         var CASES = [
-            ['1234', 1234],
-            ['01234.56', 1234.56],
-            ['.0', 0],
-            ['-1', -1],
+            ['1234',      1234],
+            ['01234.56',  1234.56],
+            ['.0',        0],
+            ['.5',        0.5],
+            ['-1',        -1],
             ['123.45abc', undefined],
             ['abc123.45', undefined],
-            ['0x123', undefined]];
-
-        for (var i = 0; i < CASES.length; ++i) {
-            var input = CASES[i][0];
-            var expected = CASES[i][1];
-            var actual = Methanal.Util.strToFloat(input);
-            self.assert(expected === actual, 'input = ' + input +
-                ' :: expected = ' + expected + ' :: actual = ' + actual);
-        }
+            ['0x123',     undefined]];
+        Methanal.Tests.Util.assertCases(self, Methanal.Util.strToFloat, CASES);
     },
 
 
