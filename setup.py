3ad3a340474081585fe7abebc712d26fbd787a81
Tristan Seligmann <mithrandi@mithrandi.net>
2011-09-01 14:15:01 +0200

    woo setup.py

--- setup.py
+++ setup.py
@@ -1,17 +1,29 @@
-from epsilon.setuphelper import autosetup
-
-import methanal
-
-autosetup(
+from setuptools import find_packages, setup
+setup(
     name='Methanal',
-    version=methanal.version.short(),
-    maintainer='Tristan Seligmann / Jonathan Jacobs',
-    maintainer_email='mithrandi@mithrandi.za.net / korpse@slipgate.za.net',
-    license='MIT',
-    platforms=['any'],
+    version='0.2.1',
+    maintainer='Methanal developers',
+    maintainer_email='methanal-dev@lists.launchpad.net',
     description='A web forms library for Mantissa',
+    url='https://launchpad.net/methanal/',
     classifiers=[
+        'Development Status :: 4 - Beta',
+        'Environment :: Web Environment',
+        'Framework :: Twisted',
         'Intended Audience :: Developers',
-        'Programming Language :: Python',
-        'Development Status :: 3 - Alpha',
-        'Topic :: Internet'])
+        'License :: OSI Approved :: MIT License',
+        'Operating System :: OS Independent',
+        'Programming Language :: JavaScript',
+        'Programming Language :: Python :: 2.5',
+        'Programming Language :: Python :: 2.6',
+        'Programming Language :: Python :: 2.7',
+        'Topic :: Internet :: WWW/HTTP',
+        'Topic :: Internet :: WWW/HTTP :: Dynamic Content',
+        'Topic :: Software Development :: Libraries :: Python Modules'],
+    packages=find_packages() + ['nevow.plugins', 'xmantissa.plugins'],
+    include_package_data=True,
+    install_requires=['Twisted >= 2.5.0',
+                      'Epsilon >= 0.5.0',
+                      'Axiom >= 0.5.20',
+                      'Nevow >= 0.9.8',
+                      'Mantissa >= 0.6.1'])
